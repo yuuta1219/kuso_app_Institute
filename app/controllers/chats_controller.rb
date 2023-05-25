3dd27e299fd7a287e092a138250d0b15f751ec90
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-05-25 23:11:05 +0900

    厳しすぎるので修正

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,12 @@
 class ChatsController < ApplicationController
   
   def index
-    random_number = rand(1..5)
+    random_number = rand(1..8)
 
     if random_number != 1
-      client = OpenAiClientDevil.new
-    else
       client = OpenAiClient.new
+    else
+      client = OpenAiClientDevil.new
     end
     synonym = params[:synonym]
     @response = client.chat(synonym)
