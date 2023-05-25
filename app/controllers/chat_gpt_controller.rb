427879cc648dce30c1f79b296f8fbe2cf40c039f
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-05-25 18:25:37 +0900

    OpenAIClien作成

--- app/controllers/chat_gpt_controller.rb
+++ app/controllers/chat_gpt_controller.rb
@@ -1,2 +1,10 @@
-class ChatGptController < ApplicationController
+class ChatsController < ApplicationController
+  
+  def index; end
+  
+  def create
+    client = OpenAIClient.new
+    synonym = params[:synonym]  # ここで、リクエストパラメータからsynonymを取得します
+    @response = client.chat(synonym)
+  end
 end
---
+++ app/services/openai_client.rb
@@ -0,0 +1,21 @@
+require 'openai'
+
+class OpenAIClient
+
+  def initialize
+    @client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
+  end
+
+  def chat(synonym)
+    additional_prompt = "次に入力された食べ物が0カロリーである説明を一見論理的であるかのようにしてください。3行でお願いします。"  
+    response = @client.chat(
+      parameters: {
+          model: "gpt-3.5-turbo",
+          messages: [
+            { role: "system", content: additional_prompt },
+            { role: "user", content: synonym },
+          ],
+      })
+    response.dig("choices", 0, "message", "content")
+  end
+end
