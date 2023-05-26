dd2892cf1e90751f61969479387597d3ffa08129
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-05-26 11:20:18 +0900

    bundle exec rails g controller users new create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, uniqueness: true
+
+  enum role: { general: 0, prisoner: 1}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root 'chats#index'
 
   get'prison', to: 'chats#prison'
