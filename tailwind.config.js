b15eb0ab95c8df9b5a8009100aa7eda170177404
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-05-26 12:05:00 +0900

    色変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,18 @@ module.exports = {
     extend: {
       animation: {
         "slide-rotate-hor-b-fwd": "slide-rotate-hor-b-fwd 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955)   both",
-        "scale-in-hor-center": "scale-in-hor-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both"
+        "scale-in-hor-center": "scale-in-hor-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
+        "color-change-2x": "color-change-2x 2s linear  infinite alternate both"
       },
       keyframes: {
+        "color-change-2x": {
+          "0%": {
+              background: "#F30100"
+          },
+          to: {
+              background: "#ffffff"
+          }
+      },
         "scale-in-hor-center": {
           "0%": {
               transform: "scaleX(0)",
