2ca2855161c4ffeef2c2b9d22e04e624b4c392d6
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-09 00:47:54 +0900

    yarn add @hotwired/turbo-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import { Turbo } from "@hotwired/turbo-rails";
+import { Controller } from "controllers";
