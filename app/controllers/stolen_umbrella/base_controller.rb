e32ee9b93359e9796b13c759eebb1c6e075ea512
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-08 20:20:28 +0900

    レイアウト色変更cupcake

--- app/controllers/stolen_umbrella/base_controller.rb
+++ app/controllers/stolen_umbrella/base_controller.rb
@@ -1,2 +1,9 @@
 class StolenUmbrella::BaseController < ApplicationController
+  before_action :calorie_theme_color
+
+  private
+
+  def calorie_theme_color
+    @theme_color = "cupcake"
+  end
 end
