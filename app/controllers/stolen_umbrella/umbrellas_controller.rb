3e7c356db61ecb15de90540a0d4d65524ef1652e
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-08 18:57:17 +0900

    umbrellaControllerの基本形を作成

--- app/controllers/stolen_umbrella/umbrellas_controller.rb
+++ app/controllers/stolen_umbrella/umbrellas_controller.rb
@@ -1,2 +1,46 @@
 class StolenUmbrella::UmbrellasController < StolenUmbrella::BaseController
+    def index
+      @umbrellas = Umbrella.all
+    end
+  
+    def new
+      @umbrella = Umbrella.new
+    end
+
+    def create
+      @umbrella = Umbrella.new(umbrella_params)
+  
+      if @umbrella.save
+        redirect_to stolen_umbrella_umbrellas_path, notice: 'Umbrella was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+      @umbrella = Umbrella.find(params[:id])
+    end
+  
+    def update
+      @umbrella = Umbrella.find(params[:id])
+  
+      if @umbrella.update(umbrella_params)
+        redirect_to stolen_umbrella_umbrellas_path, notice: 'Umbrella was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @umbrella = Umbrella.find(params[:id])
+      @umbrella.destroy
+  
+      redirect_to stolen_umbrella_umbrellas_path, notice: 'Umbrella was successfully destroyed.'
+    end
+  
+    private
+  
+    def umbrella_params
+      params.require(:umbrella).permit()
+    end
 end
