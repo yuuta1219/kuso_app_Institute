ced96aa0cc198fdb8bd9ad1c738cb9c2a20b6f0b
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-08 19:15:16 +0900

    userとumbrellaモデルの紐付け

--- app/models/umbrella.rb
+++ app/models/umbrella.rb
@@ -1,2 +1,3 @@
 class Umbrella < ApplicationRecord
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_021453) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_100939) do
+  create_table "umbrellas", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "memory", null: false
+    t.string "stolen_place"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_umbrellas_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -21,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_021453) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "umbrellas", "users"
 end
