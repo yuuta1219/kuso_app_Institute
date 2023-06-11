4cea1630d251d21a97d9f2ede88956f9248f38c0
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-11 13:16:07 +0900

    hotwierの練習で傘アプリの修正

--- app/controllers/stolen_umbrella/umbrellas_controller.rb
+++ app/controllers/stolen_umbrella/umbrellas_controller.rb
@@ -4,18 +4,14 @@ class StolenUmbrella::UmbrellasController < StolenUmbrella::BaseController
     @umbrellas = Umbrella.order(created_at: :desc).limit(30)
     @umbrella = Umbrella.new
   end
-
-  def new
-    @umbrella = Umbrella.new
-  end
-
+  
   def create
     @umbrella = Umbrella.create(umbrella_params)
 
     if @umbrella.save
       redirect_to stolen_umbrella_umbrellas_path, notice: 'Umbrella was successfully created.'
     else
-      redirect_to stolen_umbrella_umbrellas_path
+    render turbo_stream: turbo_stream.replace('error_messages', partial: 'shared/error_messages', locals: { object: @umbrella })
     end
   end
 
