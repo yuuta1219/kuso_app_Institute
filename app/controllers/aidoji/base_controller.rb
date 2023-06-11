3be0ad397cd9f2f61c6c75d326e7a1e984b4dc9c
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-12 05:49:32 +0900

    それぞれのコントローラーのカラーメソッド名修正

--- app/controllers/aidoji/base_controller.rb
+++ app/controllers/aidoji/base_controller.rb
@@ -1,9 +1,9 @@
 class Aidoji::BaseController < ApplicationController
-  before_action :calorie_theme_color
+  before_action :aidoji_theme_color
 
   private
 
-  def calorie_theme_color
-    @theme_color = "aespa"
+  def aidoji_theme_color
+    @theme_color = "valentine"
   end
 end
