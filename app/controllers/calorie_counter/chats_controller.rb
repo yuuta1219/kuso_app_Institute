92f041c1f4e19c44067418bf9f377c8725dc56ee
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-13 19:05:33 +0900

    カロリーカウンター処理が遅いので修正

--- app/controllers/calorie_counter/chats_controller.rb
+++ app/controllers/calorie_counter/chats_controller.rb
@@ -2,10 +2,12 @@ class CalorieCounter::ChatsController < CalorieCounter::BaseController
   rescue_from RuntimeError, with: :handle_unwanted_word
   before_action :prisoner_go_to_prison , only: [:index]
   before_action :not_prison, only: [:purgatory]
-  
+
   def index
-    random_number = rand(1..10)
     input = CalorieCounter::WordChecker.check_input(params[:input])
+    return if input.nil?
+
+    random_number = rand(1..10)
     if random_number != 1
       client = CalorieCounter::OpenAiClient.new
     else
@@ -13,7 +15,7 @@ class CalorieCounter::ChatsController < CalorieCounter::BaseController
     end
     @response = client.chat(input)
   end
-  
+
   def prison
     user = User.create_prisoner
     auto_login(user)
