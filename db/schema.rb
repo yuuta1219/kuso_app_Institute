1caa73c1213d2bf17dda3d3ae225e404deb7fc54
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-20 09:56:20 +0900

    rails g models song

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_11_222015) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_002119) do
+  create_table "songs", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
