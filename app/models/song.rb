3d3d07710bf13a80561ce2f1615308c02f5a26b9
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-23 17:17:28 +0900

    データベース修正

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
-  has_many  :song_tags
   has_many :tags, through: :song_tags
+  has_many :song_tags, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :song_tags
+  has_many :song_tags, dependent: :destroy
   has_many :songs, through: :song_tags
 
   validates :title, presence: true
