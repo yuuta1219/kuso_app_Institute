150fee32ec6fd945e627c6d0de974c826a56785e
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-06-26 19:36:23 +0900

    tesy

--- app/controllers/calorie_counter/base_controller.rb
+++ app/controllers/calorie_counter/base_controller.rb
@@ -6,11 +6,11 @@ class CalorieCounter::BaseController < ApplicationController
   def calorie_theme_color
     @theme_color = "synthwave"
   end
-  
+
   def prisoner_go_to_prison
     redirect_to purgatory_path if current_user && current_user.prisoner?
   end
-  
+
   def not_prison
     redirect_to calorie_counter_chats_path unless current_user || current_user && current_user.prisoner? 
   end
