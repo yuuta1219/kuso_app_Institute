122b12018a0350a8fab840a5725d44b0843d1bed
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-07-30 23:02:22 +0900

    e

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -4,10 +4,10 @@ class TopsController < ApplicationController
   def top
     @color_select = "approval"
     set_meta_tags twitter: {
-      card: "summary",
+      card: "summary_large_image",
       url: "https://kuso-app-institute.magia.runteq.jp/",
       title: "K.U.S.O.AppInstitute",
-      image: ActionController::Base.helpers.image_url(['ogp1.png', 'ogp2.png'].sample),
+      image: ["https://kuso-app-institute.magia.runteq.jp/images/ogp1.png", "https://kuso-app-institute.magia.runteq.jp/images/ogp2.png"].sample,
       description: "クソアプリ研究所を発見したよ！"
     }
   end
